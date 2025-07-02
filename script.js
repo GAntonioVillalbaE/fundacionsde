7820a0ccc23f28485895773023422c7821a8a37c
Guillermo Antonio Villalba Escamilla <guillermovillalba0501@gmail.com>
2025-07-01 21:54:49 -0700

    1-07-2025 Ver 2

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function toggleMenu() {
-  const menu = document.getElementById("menutop");
-  menu.classList.toggle("active");
-}
+const menuIcono = document.querySelector(".menu-movil-icono");
+const menuMovil = document.querySelector(".menu-sde");
+
+menuIcono.addEventListener("click", () => {
+    menuIcono.classList.toggle("active");
+    menuMovil.classList.toggle("active");
+});
